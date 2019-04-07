aae8263bf37e9f1a168fd3de80b1568540070a06
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2019-04-07 16:08:36 +0200

    button A starts a setnence

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+let CountM1 = 0
+let CountM3 = 0
+
+
 basic.showLeds(`
     . . . . .
     . # . # .
@@ -5,3 +9,46 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `);
+
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . . # . .
+    . . # . .
+    `)
+    CountM1 = 0
+    CountM3 = 0
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 255)
+    basic.pause(2000)
+    basic.showLeds(`
+    . . # . .
+    . . # . .
+    # . # . #
+    . # # # .
+    . . # . .
+    `)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 255)
+    basic.pause(2000)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 255)
+    basic.pause(2000)
+    motor.motorStopAll()
+    basic.showIcon(IconNames.Asleep)
+    basic.showString("M1=")
+    basic.showNumber(CountM1)
+    basic.pause(1000)
+    basic.showString("M3=")
+    basic.showNumber(CountM3)
+})
+
+input.onPinPressed(TouchPin.P0, function () {
+    CountM1 += 1
+})
+
+input.onPinPressed(TouchPin.P1, function () {
+    CountM3 += 1
+})
